0494cc666fa2d345ad40c96bd6b4a81cd72344de
vikasAr <vikasaroorme111@gmail.com>
2025-08-16 19:44:52 +0530

    add gitcheckouy.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,1 +1,18 @@
+def call(Map gitcred) 
+    checkout scmGit(branches: [[name: '*/gitcred.branch']], 
+    extensions: [], 
+    userRemoteConfigs: [[credentialsId: 'gitcred.credentials', url: 'gitcred.myurl']])
 
+
+def gitcred {
+    branch = main
+    credentials = github-my
+    myurl = https://github.com/vikasaroor/delete.git
+
+}
+
+
+// def call(Map gitcred) 
+//     checkout scmGit(branches: [[name: '*/gitcred.branch']], 
+//     extensions: [], 
+//     userRemoteConfigs: [[credentialsId: 'github-my', url: 'https://github.com/vikasaroor/delete.git']])
